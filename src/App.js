426cfa7a6810244cb28485f113c5f3a73d1dd24c
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-04-20 17:11:27 +0430

    redux toolkit set

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { hi, hi2 } from './features/counter/counterSlice';
 
 function App() {
+  const dispatch = useDispatch();
+  const data = useSelector((store) => store);
+  console.log(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>
+        Hi,
+      </h2>
+      <button type="button" onClick={() => dispatch(hi())}>Update</button>
+      <button type="button" onClick={() => dispatch(hi2())}>reset</button>
     </div>
   );
 }
